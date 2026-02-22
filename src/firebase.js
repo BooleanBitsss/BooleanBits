09acf4506de1493c1781f2c6b3f2f5ce8450c279
BooleanBitsss <booleanBitsss@gmail.com>
2026-02-22 20:30:15 +0530

    ok

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBgo0LbaDCSyGGhrO8VTZ3v3nzh9Z6mON8",
-  authDomain: "booleanbitss.firebaseapp.com",
-  projectId: "booleanbitss",
-  storageBucket: "booleanbitss.firebasestorage.app",
-  messagingSenderId: "149738842499",
-  appId: "1:149738842499:web:b7244ed56d5f31e611eeb6",
-  measurementId: "G-52ZF1K2MLJ"
+  apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+
 };
 
 // Initialize Firebase
